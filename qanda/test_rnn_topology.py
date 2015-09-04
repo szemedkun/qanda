a309ab5c42edaa2f968003ec7c79c64649106a57
Samuel Zemedkun <samuel.ejigu@gmail.com>
2015-09-04 11:11:48 -0600

    Grid searching on sent and query length added

---
+++ qanda/test_rnn_topology.py
@@ -0,0 +1,18 @@
+from datasets import *
+from fit import *
+
+ds = Datasets(task_index = 1, only_supporting = False, similar_only = False, min_num = 2)
+ds.fit()
+
+X, qX, Y = ds.get_training_data()
+tX, tXq, tY = ds.get_testing_data()
+
+accuracy_bucket = []
+for shs in [100, 500, 1000]:
+	for qhs in [50, 100, 200]:
+		model_lstm = Fit( vocab_size = ds.vocab_size, sent_hidden_size = shs, query_hidden_size = qhs )
+		model_lstm.compile_layers()
+		model_lstm.run(X, qX, Y)
+		model_accu = model_lstm.score(tX, tXq, tY)
+		accuracy_bucket.append( (shs, qhs, model_accu) )
+print accuracy_bucket
