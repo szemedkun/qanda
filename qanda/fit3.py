b13effb57ba64c26839b4d97e3b5e9ebf1278c42
Samuel Zemedkun <samuel.ejigu@gmail.com>
2015-09-08 13:20:20 -0600

    changed parameters

--- qanda/fit3.py
+++ qanda/fit3.py
@@ -157,7 +157,7 @@ if __name__ == "__main__":
 												  new_dict[8], \
 												  new_dict[9]
 
-	model_lstm = Fit3( vocab_size = ds.vocab_size, sent_hidden_size = 100, query_hidden_size = 20 )
+	model_lstm = Fit3( vocab_size = ds.vocab_size, sent_hidden_size = 500, query_hidden_size = 100 )
 	model_lstm.compile_layers()
-	model_lstm.run(X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, Xq, Y)
+	model_lstm.run(X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, qX, Y)
 	print model_lstm.score(tX1, tX2, tX3, tX4, tX5, tX6, tX7, tX8, tX9, tX10, tXq, tY)
