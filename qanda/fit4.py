0adaecb413b12a17b9bab4ae4d22cd96e198dd68
Samuel Zemedkun <samuel.ejigu@gmail.com>
2015-09-09 14:18:33 -0600

    parameter updated

--- qanda/fit4.py
+++ qanda/fit4.py
@@ -126,7 +126,7 @@ class Fit4(object):
 
 
 if __name__ == "__main__":
-	ds = Datasets(task_index = 1, only_supporting = False, use10k = False)
+	ds = Datasets(task_index = 2, only_supporting = False, use10k = False)
 	ds.fit()
 
 	X, qX, Y = ds.get_training_data()
@@ -164,7 +164,7 @@ if __name__ == "__main__":
 												  new_dict[8], \
 												  new_dict[9]
 
-	model4 = Fit4( vocab_size = ds.vocab_size, sent_hidden_size = 1000, query_hidden_size = 20
+	model4 = Fit4( vocab_size = ds.vocab_size, sent_hidden_size = 500, query_hidden_size = 100
 	, model = recurrent.LSTM )
 	model4.compile_layers()
 	model4.run(X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, qX, Y)
