77489f284a08c0d2b386119545b7318afb39d756
Samuel Zemedkun <samuel.ejigu@gmail.com>
2015-09-13 01:27:55 -0600

    added scan_for_parameters

--- qanda/scan_for_parameters.py
+++ qanda/scan_for_parameters.py
@@ -23,26 +23,28 @@ def scan_for_parameters():
 	# qhss = [2,3,10,20,100]
 	qhss = [10]
 
-	for shs in shss:
-		for qhs in qhss:
-			model_lstm = Fit( vocab_size = ds.answers_size
-				, batch_size =16
-				, epochs = 10
-				, sent_hidden_size = shs
-				, query_hidden_size = qhs 
-				)
+	for min_num in [1,2,3,4,5,6,7,8]:
 
-			model_lstm.compile_layers()
-			model_lstm.run(X, qX, Y)
-			accuracy = model_lstm.score(tX, tXq, tY)
+		for shs in shss:
+			for qhs in qhss:
+				model_lstm = Fit( vocab_size = ds.answers_size
+					, batch_size =16
+					, epochs = 10
+					, sent_hidden_size = shs
+					, query_hidden_size = qhs 
+					)
 
-			accum = compare_predictions(ds, model_lstm, tX, tXq, tY)
-			model_plus_pred = [accum, model_lstm, accuracy]
+				model_lstm.compile_layers()
+				model_lstm.run(X, qX, Y)
+				accuracy = model_lstm.score(tX, tXq, tY)
 
-			# file_name = '../../pickled_models/sent_size/model_lstm_shs_{}_task1_using_10k_qhs_{}.pkl'.format(shs, qhs)
-			# print('Pickling model ...')
-			# with open(file_name,'wb') as f:
-			# 	pkl.dump(model_plus_pred, f)
+				accum = compare_predictions(ds, model_lstm, tX, tXq, tY)
+				model_plus_pred = [accum, model_lstm, accuracy]
+
+				file_name = '../../pickled_models/gensim_hack/model_task1_using_10k_min_num_{}.pkl'.format(min_num)
+				print('Pickling model ...')
+				with open(file_name,'wb') as f:
+					pkl.dump(model_plus_pred, f)
 
 	# dropouts = [0.1, 0.2, 0.4]
 	# for dp in dropouts:
